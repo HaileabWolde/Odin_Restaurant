35cb62a9e282ac372dcb9e06f22925b4f95feb62
Haileab <womahaileab1428@gmail.com>
2025-06-18 20:58:46 +0300

    Added the UI for Menu Section

--- src/Menu.js
+++ src/Menu.js
@@ -1,16 +1,21 @@
 import backgroundImage from './image/Background.jpg';
+import Menu from './Menu.json'
 const image = document.createElement('img');
 image.src = backgroundImage;
 const MenuPage = (function () {
     const newDiv = document.createElement('div');
     newDiv.classList.add('menu');
      newDiv.id = 'menu-page';
-    newDiv.innerHTML = `
-        <h1>Welcome to Odin Restaurant and Let's Fuck</h1>
-        <p>Experience the best culinary delights crafted with passion and expertise.</p>
-        <p>Explore our menu, contact us for reservations, or simply enjoy the ambiance.</p>
-        <img src="${image.src}" alt="Restaurant Background" class="background-image">
+   Menu.map((item)=>{
+    const MenuItem = document.createElement('div');
+    MenuItem.classList.add('menu-item');
+    MenuItem.innerHTML = `
+        <h2>${item.name}</h2>
+        <p>${item.description}</p>
+        <p>Price: $${item.price.toFixed(2)}</p>
     `;
+    return newDiv.appendChild(MenuItem);
+   })
    
     return {
         render: () => {
